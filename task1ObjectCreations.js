6e9aae1971224d8b7b43c9977e3c8576e217c1f0
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-03-30 18:16:40 +0300

    add resolved task

--- task1ObjectCreations.js
+++ task1ObjectCreations.js
@@ -1,1 +1,14 @@
+// Задача 2: Создание объекта пользователя
 
+// + Создаём пустой объект user
+let user = {};
+// + Добавляем свойство name
+user.name = "John";
+// + Добавляем свойство surname
+user.surname = "Smith";
+
+// + Изменяем значение name
+user.name = "Pete";
+// + Удаляем свойство name
+delete user.name;
+console.log(user)
