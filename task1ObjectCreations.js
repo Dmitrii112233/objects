fd4868f2d98bda2a26141e62466bb50ce0a9c544
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-03-30 18:17:01 +0300

    add console log to view realtime changes

--- task1ObjectCreations.js
+++ task1ObjectCreations.js
@@ -2,13 +2,16 @@
 
 // + Создаём пустой объект user
 let user = {};
+console.log(user)
 // + Добавляем свойство name
 user.name = "John";
+console.log(user)
 // + Добавляем свойство surname
 user.surname = "Smith";
-
+console.log(user)
 // + Изменяем значение name
 user.name = "Pete";
+console.log(user)
 // + Удаляем свойство name
 delete user.name;
 console.log(user)
